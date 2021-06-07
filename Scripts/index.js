3d3c3a08b68f8316bfa599ee318b8551b3bbe746
Charptr0 <CharlesOfficial1@hotmail.com>
2021-06-07 19:29:19 -0400

    ocatve c2 completed

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,26 +1,93 @@
-import * as keys from "./piano.js"
+import * as notes from "./piano.js"
 
 let piano = document.getElementById("piano");
-const DEFUALT_OCTAVE = keys.OCTAVES["C4"];
+const DEFUALT_OCTAVE = notes.OCTAVES["C2"];
 let current_octave = DEFUALT_OCTAVE;
 
+document.getElementById("octave-lv").innerHTML = "Current Octave: " + notes.LIST_OF_ALL_OCTAVES[current_octave];
+
 document.addEventListener("keydown", logKey);
-document.addEventListener("keyup", () =>{piano.src = keys.PIANO_C2_OCTAVE;})
+document.addEventListener("keyup", () =>{piano.src = notes.PIANO_C2_OCTAVE;})
 
 function logKey(e)
 {
     switch(e.code)
     {
         case "Digit1":
-            piano.src = keys.PIANO_C2_PRESSED;
+            piano.src = notes.PIANO_C2_PRESSED;
             break;
 
         case "Digit2":
-            piano.src = keys.PIANO_D2_PRESSED;
+            piano.src = notes.PIANO_D2_PRESSED;
+            break;
+
+        case "Digit3":
+            piano.src = notes.PIANO_E2_PRESSED;
+            break;
+
+        case "Digit4":
+            piano.src = notes.PIANO_F2_PRESSED;
+            break;
+
+        case "Digit5":
+            piano.src = notes.PIANO_G2_PRESSED;
+            break;
+
+        case "Digit6":
+            piano.src = notes.PIANO_A2_PRESSED;
+            break;
+        
+        case "Digit7":
+            piano.src = notes.PIANO_B2_PRESSED;
+            break;
+
+        case "Digit8":
+            piano.src = notes.PIANO_C3_PRESSED;
+            break;
+
+        case "Digit9":
+            piano.src = notes.PIANO_D3_PRESSED;
+            break;
+
+        case "Digit0":
+            piano.src = notes.PIANO_E3_PRESSED;
+            break;
+
+        case "KeyQ":
+            piano.src = notes.PIANO_CS2_PRESSED;
+            break;
+
+        case "KeyW":
+            piano.src = notes.PIANO_Eb2_PRESSED;
+            break;
+
+        case "KeyE":
+            piano.src = notes.PIANO_FS2_PRESSED;
+            break;
+
+        case "KeyR":
+            piano.src = notes.PIANO_Ab2_PRESSED;
+            break;
+
+        case "KeyT":
+            piano.src = notes.PIANO_Bb2_PRESSED;
+            break;
+
+        case "KeyY":
+            piano.src = notes.PIANO_CS3_PRESSED;
+            break;
+
+        case "KeyU":
+            piano.src = notes.PIANO_Eb3_PRESSED;
             break;
 
         default:
-            console.log(e.code);
             break;
     }
+}
+
+function octaveChanger()
+{
+    let octave_level = document.getElementById("octave-lv");
+    octave_level.innerHTML = "Current Octave: " + notes.LIST_OF_ALL_OCTAVES[current_octave];
 }
