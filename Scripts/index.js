4369150d42bdc7e572e353c3a3ee900ae7c44c20
Charptr0 <CharlesOfficial1@hotmail.com>
2021-06-07 21:15:08 -0400

    added audio for most C2 octave

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,13 +1,26 @@
-import * as notes from "./piano.js"
+import * as notes from "./piano_notes.js"
+import * as sounds from "./piano_sounds.js"
 
 let piano = document.getElementById("piano");
 const DEFUALT_OCTAVE = notes.OCTAVES["C2"];
 let current_octave = DEFUALT_OCTAVE;
+let current_playing_note = new Audio();
 
 document.getElementById("octave-lv").innerHTML = "Current Octave: " + notes.LIST_OF_ALL_OCTAVES[current_octave];
 
 document.addEventListener("keydown", logKey);
-document.addEventListener("keyup", () =>{piano.src = notes.PIANO_C2_OCTAVE;})
+document.addEventListener("keyup", () =>{
+    piano.src = notes.PIANO_C2_OCTAVE;
+
+})
+
+function playAudio(key_audio)
+{
+    current_playing_note.pause();
+    current_playing_note.currentTime = 0;
+    current_playing_note = key_audio;
+    current_playing_note.play();
+}
 
 function logKey(e)
 {
@@ -15,46 +28,57 @@ function logKey(e)
     {
         case "Digit1":
             piano.src = notes.PIANO_C2_PRESSED;
+            playAudio(sounds.C2_AUDIO);
             break;
 
         case "Digit2":
             piano.src = notes.PIANO_D2_PRESSED;
+            playAudio(sounds.D2_AUDIO);
             break;
 
         case "Digit3":
             piano.src = notes.PIANO_E2_PRESSED;
+            playAudio(sounds.E2_AUDIO);
             break;
 
         case "Digit4":
             piano.src = notes.PIANO_F2_PRESSED;
+            playAudio(sounds.F2_AUDIO);
             break;
 
         case "Digit5":
             piano.src = notes.PIANO_G2_PRESSED;
+            playAudio(sounds.G2_AUDIO);
             break;
 
         case "Digit6":
             piano.src = notes.PIANO_A2_PRESSED;
+            playAudio(sounds.A2_AUDIO);
             break;
         
         case "Digit7":
             piano.src = notes.PIANO_B2_PRESSED;
+            playAudio(sounds.B2_AUDIO);
             break;
 
         case "Digit8":
             piano.src = notes.PIANO_C3_PRESSED;
+            playAudio(sounds.C3_AUDIO);
             break;
 
         case "Digit9":
             piano.src = notes.PIANO_D3_PRESSED;
+            playAudio(sounds.D3_AUDIO);
             break;
 
         case "Digit0":
             piano.src = notes.PIANO_E3_PRESSED;
+            playAudio(sounds.E3_AUDIO);
             break;
 
         case "KeyQ":
             piano.src = notes.PIANO_CS2_PRESSED;
+            playAudio(sounds.CS2_AUDIO);
             break;
 
         case "KeyW":
---
+++ Scripts/piano_sounds.js
@@ -0,0 +1,23 @@
+function audioGenerator(FILE_PATH)
+{
+    return new Audio(FILE_PATH);
+}
+
+
+export const C2_AUDIO = audioGenerator("./Sounds/C2.mp3");
+export const CS2_AUDIO = audioGenerator("./Sounds/CS2.mp3");
+export const D2_AUDIO = audioGenerator("./Sounds/D2.mp3");
+export const Eb2_AUDIO = audioGenerator("./Sounds/Eb2.mp3");
+export const E2_AUDIO = audioGenerator("./Sounds/E2.mp3");
+export const F2_AUDIO = audioGenerator("./Sounds/F2.mp3");
+export const FS2_AUDIO = audioGenerator("./Sounds/FS2.mp3");
+export const G2_AUDIO = audioGenerator("./Sounds/G2.mp3");
+export const Ab2_AUDIO = audioGenerator("./Sounds/Ab2.mp3");
+export const A2_AUDIO = audioGenerator("./Sounds/A2.mp3");
+export const Bb2_AUDIO = audioGenerator("./Sounds/Bb2.mp3");
+export const B2_AUDIO = audioGenerator("./Sounds/B2.mp3");
+export const C3_AUDIO = audioGenerator("./Sounds/C3.mp3");
+export const CS3_AUDIO = audioGenerator("./Sounds/CS3.mp3");
+export const D3_AUDIO = audioGenerator("./Sounds/D3.mp3");
+export const Eb3_AUDIO = audioGenerator("./Sounds/Eb3.mp3");
+export const E3_AUDIO = audioGenerator("./Sounds/E3.mp3");
